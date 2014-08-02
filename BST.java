835a8801379ae1c1a98bc80d3d39bfde56da1f02
Alexis Chantreau <achantreau@hotmail.fr>
2014-08-02 14:42:45 +0100

    Cleaning up BST.java code and added .gitignore file.

--- BST.java
+++ BST.java
@@ -1,73 +1,139 @@
+
 /**
+ * Simplistic implementation of Binary Search Trees in Java. Designed as an API
+ * to create binary search trees in the code, and to encode them in a JSON
+ * format, so that the trees can be visualised in the Javascript
+ * BinaryTreeVisualizer.
  *
- * @author achantreau
+ * @author achantreau Copyright (c) 2014
+ * @param <K> key type of the binary search tree.
  */
 public class BST<K extends Comparable<K>> {
 
+    /**
+     * Root node of binary search tree.
+     */
     private BSTNode<K> root;
 
-    public BST() {
-        root = null;
+    /**
+     * Node of binary search tree.
+     *
+     * @param <K> key type of the binary tree.
+     */
+    private class BSTNode<K> {
+
+        public K key;
+        public BSTNode<K> left;
+        public BSTNode<K> right;
+
+        public BSTNode(K key, BSTNode<K> left, BSTNode<K> right) {
+            this.key = key;
+            this.left = left;
+            this.right = right;
+        }
+
+        public BSTNode(K key) {
+            this(key, null, null);
+        }
     }
 
+    /**
+     * Inserts a new node in the tree using an iterative approach. The node will
+     * be inserted in the appropriate position to maintain the characteristic
+     * property of binary search trees.
+     *
+     * @param key the key of the node to insert.
+     */
     public void insert(K key) {
-        root = insert(root, key);
-    }
+        BSTNode<K> parent = null;
+        BSTNode<K> current = root;
+        BSTNode<K> newNode = new BSTNode<>(key);
 
-    private BSTNode<K> insert(BSTNode<K> n, K key) {
-        if (n == null) {
-            return new BSTNode<>(key, null, null);
+        while (current != null) {
+            parent = current;
+
+            if (key.compareTo(current.key) < 0) {
+                current = current.left;
+            } else if (key.compareTo(current.key) > 0) {
+                current = current.right;
+            }
         }
 
-        if (key.compareTo(n.getKey()) < 0) {
-            n.setLeft(insert(n.getLeft(), key));
-            return n;
+        if (parent == null) {
+            root = newNode;
         } else {
-            n.setRight(insert(n.getRight(), key));
-            return n;
+            if (key.compareTo(parent.key) < 0) {
+                parent.left = newNode;
+            } else if (key.compareTo(parent.key) > 0) {
+                parent.right = newNode;
+            }
         }
     }
 
+    /**
+     * Returns the space separated String, representing the in-order traversal
+     * of the binary search tree.
+     * 
+     * @return the in-order traversal of the binary search tree.
+     */
     public String inOrderTraversal() {
         StringBuilder sb = new StringBuilder();
         inOrderTraversal(root, sb);
-        return sb.toString().substring(0, sb.toString().length() - 2);
+        return sb.toString();
     }
 
     private void inOrderTraversal(BSTNode<K> root, StringBuilder sb) {
-        if (root != null) {
-            inOrderTraversal(root.getLeft(), sb);
-            sb.append(root.getKey()).append(", ");
-            inOrderTraversal(root.getRight(), sb);
+        if (root == null) {
+            return;
         }
+
+        inOrderTraversal(root.left, sb);
+        sb.append(root.key).append(" ");
+        inOrderTraversal(root.right, sb);
     }
 
+    /**
+     * Returns the space separated String, representing the pre-order traversal
+     * of the binary search tree.
+     * 
+     * @return the pre-order traversal of the binary search tree.
+     */
     public String preOrderTraversal() {
         StringBuilder sb = new StringBuilder();
         preOrderTraversal(root, sb);
-        return sb.toString().substring(0, sb.toString().length() - 2);
+        return sb.toString();
     }
 
     private void preOrderTraversal(BSTNode<K> root, StringBuilder sb) {
-        if (root != null) {
-            sb.append(root.getKey()).append(", ");
-            preOrderTraversal(root.getLeft(), sb);
-            preOrderTraversal(root.getRight(), sb);
+        if (root == null) {
+            return;
         }
+
+        sb.append(root.key).append(" ");
+        preOrderTraversal(root.left, sb);
+        preOrderTraversal(root.right, sb);
     }
 
+    /**
+     * Returns the space separated String, representing the post-order traversal
+     * of the binary search tree.
+     * 
+     * @return the post-order traversal of the binary search tree.
+     */
     public String postOrderTraversal() {
         StringBuilder sb = new StringBuilder();
         postOrderTraversal(root, sb);
-        return sb.toString().substring(0, sb.toString().length() - 2);
+        return sb.toString();
     }
 
     private void postOrderTraversal(BSTNode<K> root, StringBuilder sb) {
-        if (root != null) {
-            postOrderTraversal(root.getLeft(), sb);
-            postOrderTraversal(root.getRight(), sb);
-            sb.append(root.getKey()).append(", ");
+        if (root == null) {
+            return;
         }
+
+        postOrderTraversal(root.left, sb);
+        postOrderTraversal(root.right, sb);
+        sb.append(root.key).append(" ");
     }
 
     public String levelOrderTraversal() {
@@ -75,16 +141,16 @@ public class BST<K extends Comparable<K>> {
 
         int height = height();
         for (int i = 1; i <= height; i++) {
-            sb.append(givenLevelAsString(i)).append(", ");
+            sb.append(givenLevelAsString(i)).append(" ");
         }
 
-        return sb.toString().substring(0, sb.toString().length() - 2);
+        return sb.toString();
     }
 
     public String givenLevelAsString(int level) {
         StringBuilder sb = new StringBuilder();
         givenLevelAsString(root, level, sb);
-        return sb.toString().substring(0, sb.toString().length() - 2);
+        return sb.toString();
     }
 
     private void givenLevelAsString(BSTNode<K> root, int level, StringBuilder sb) {
@@ -92,10 +158,10 @@ public class BST<K extends Comparable<K>> {
             sb.append("");
         } else {
             if (level == 1) {
-                sb.append(root.getKey()).append(", ");
+                sb.append(root.key).append(" ");
             } else if (level > 1) {
-                givenLevelAsString(root.getLeft(), level - 1, sb);
-                givenLevelAsString(root.getRight(), level - 1, sb);
+                givenLevelAsString(root.left, level - 1, sb);
+                givenLevelAsString(root.right, level - 1, sb);
             }
         }
     }
@@ -106,103 +172,122 @@ public class BST<K extends Comparable<K>> {
         }
     }
 
+    /**
+     * Returns the number of nodes in the binary search tree.
+     *
+     * @return the number of nodes in the binary search tree.
+     */
+    public int size() {
+        return size(root);
+    }
+
+    /**
+     * Returns the number of nodes of a particular subtree.
+     *
+     * @param root the root node of the subtree.
+     * @return the number of nodes in the subtree.
+     */
+    private int size(BSTNode<K> root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return size(root.left) + 1 + size(root.right);
+    }
+
+    /**
+     * Returns the height of the binary search tree.
+     *
+     * @return the height of the binary search tree.
+     */
     public int height() {
         return height(root);
     }
 
+    /**
+     * Returns the height of a particular subtree.
+     *
+     * @param root the root node of the subtree.
+     * @return the height of the subtree.
+     */
     private int height(BSTNode<K> root) {
         if (root == null) {
             return 0;
-        } else {
-            int leftHeight = height(root.getLeft());
-            int rightHeight = height(root.getRight());
-
-            if (leftHeight > rightHeight) {
-                return (leftHeight + 1);
-            } else {
-                return (rightHeight + 1);
-            }
         }
+
+        return Math.max(height(root.left) + 1, height(root.right) + 1);
     }
 
+    /**
+     * Returns the JSON String representation of the binary search tree.
+     *
+     * @return the JSON representation of the binary search tree.
+     */
     public String toJSON() {
         StringBuilder sb = new StringBuilder();
         toJSON(root, sb);
         return sb.toString() + ";";
     }
-    
+
     private void toJSON(BSTNode<K> root, StringBuilder sb) {
-        if ((root.getLeft() != null) || (root.getRight() != null)) {
+        if ((root.left != null) || (root.right != null)) {
             sb.append("{\n" + "        id: \"")
-                    .append(root.getKey()).append("00")
+                    .append(root.key).append("00")
                     .append("\",\n" + "        name: \"")
-                    .append(root.getKey())
+                    .append(root.key)
                     .append("\",\n")
                     .append("        data: {},\n")
                     .append("        children: [");
-            if (root.getLeft() != null) {
-                toJSON(root.getLeft(), sb);
+            if (root.left != null) {
+                toJSON(root.left, sb);
                 sb.append(", ");
             } else {
                 sb.append("{\n" + "        id: \"")
-                    .append((Integer) root.getKey() - 1).append("90")
-                    .append("\",\n" + "        name: \"null")
-                    .append("\",\n")
-                    .append("        data: {},\n")
-                    .append("        children: []},");
+                        .append((Integer) root.key - 1).append("90")
+                        .append("\",\n" + "        name: \"null")
+                        .append("\",\n")
+                        .append("        data: {},\n")
+                        .append("        children: []},");
             }
-            if (root.getRight() != null) {
-                toJSON(root.getRight(), sb);
+            if (root.right != null) {
+                toJSON(root.right, sb);
             } else {
                 sb.append("{\n" + "        id: \"")
-                    .append((Integer) root.getKey() + 1).append("90")
-                    .append("\",\n" + "        name: \"null")
-                    .append("\",\n")
-                    .append("        data: {},\n")
-                    .append("        children: []}");
+                        .append((Integer) root.key + 1).append("90")
+                        .append("\",\n" + "        name: \"null")
+                        .append("\",\n")
+                        .append("        data: {},\n")
+                        .append("        children: []}");
             }
             sb.append("]}");
         } else {
             sb.append("{\n" + "        id: \"")
-                    .append(root.getKey()).append("00")
+                    .append(root.key).append("00")
                     .append("\",\n" + "        name: \"")
-                    .append(root.getKey())
+                    .append(root.key)
                     .append("\",\n")
                     .append("        data: {},\n")
                     .append("        children: []\n    }");
         }
     }
 
-    private class BSTNode<K> {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
 
-        private K key;
-        private BSTNode<K> left;
-        private BSTNode<K> right;       
+        bst.insert(30);
+        bst.insert(25);
+        bst.insert(35);
+        bst.insert(20);
+        bst.insert(26);
+        bst.insert(21);
+        bst.insert(27);
+        bst.insert(33);
+        bst.insert(38);
+        bst.insert(40);
 
-        public BSTNode(K key, BSTNode<K> left, BSTNode<K> right) {
-            this.key = key;
-            this.left = left;
-            this.right = right;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public BSTNode<K> getLeft() {
-            return left;
-        }
-
-        public void setLeft(BSTNode<K> left) {
-            this.left = left;
-        }
-
-        public BSTNode<K> getRight() {
-            return right;
-        }
-
-        public void setRight(BSTNode<K> right) {
-            this.right = right;
-        }
+        bst.printAllLevels();
+        System.out.println(bst.inOrderTraversal());
+        System.out.println(bst.toJSON());
+        System.out.println(bst.height());
     }
 }
